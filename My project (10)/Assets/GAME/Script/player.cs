35df4ada6f3d43db9189f225ebd621bb008b47a5
Owen Atkinson <45954357+Keatoi@users.noreply.github.com>
2024-11-20 20:48:59 +0000

    Score system overhaul

--- My project (10)/Assets/GAME/Script/player.cs
+++ My project (10)/Assets/GAME/Script/player.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public delegate void OnDropAction(int scoreAmount);
+    public static event OnDropAction OnDrop;
     public FruitGen FruitGen;
     public GameObject Heldfruit;
     public Transform fruithandle;
@@ -32,8 +35,15 @@ public class player : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.E)) {
            Heldfruit.GetComponent<Rigidbody2D>().simulated = true;
+            Fruitinfo fruitinfo = Heldfruit.GetComponent<Fruitinfo>();
+            if (fruitinfo != null)
+            {
+                OnDrop?.Invoke(fruitinfo.fruitindex);
+            }
+            
             Heldfruit.transform.SetParent(null);
             Heldfruit = null;
+            
             StartCoroutine(FruitGen.Genfruit());    
         }
     }
